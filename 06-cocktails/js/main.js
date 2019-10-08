f36a155d5c9373f3b395ed07891fe7893fc69c9a
jesus-crysist <jesus.crysist@gmail.com>
2019-10-08 14:57:08 +0200

    Lesson 06 - Cocktail search - framework

---
+++ 06-cocktails/js/main.js
@@ -0,0 +1,13 @@
+function searchCocktails(filter) {
+
+  const cocktailsUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?';
+
+  // get param for cocktail name - "s"
+  // get param for ingredient name - "i"
+
+}
+
+function listIngredients() {
+
+  const ingredientListUrl = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
+}
